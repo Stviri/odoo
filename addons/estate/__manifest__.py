bedc9337d7b66bc03a989d0d20ba239e4518b8e8
Stviri <stviri1998@gmail.com>
2024-02-22 16:46:46 +0400

    added access rights to the base.group users

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':"Real Estate",
+    'author':"Stviri",
+    'depends': ['base'],
+    'application': True,
+    'category':"Services",
+    'data':[
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'version':"1.0.0"
+
+}
