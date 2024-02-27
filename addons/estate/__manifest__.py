40cddaf484bfe00f2ffbeaf7180a5bed7a173d65
Stviri <stviri1998@gmail.com>
2024-02-27 18:20:15 +0400

    Added Estate Properties Functionalities

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -5,7 +5,9 @@
     'application': True,
     'category':"Services",
     'data':[
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'installable': True,
     'version':"1.0.0"
--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -1,29 +1,31 @@
 from odoo import models, fields
-
+from datetime import datetime
+# datetime გამოიყენება იმისთვის რომ როდესაც მოხდება განცხადების დადება შესაბამისი დღის თარიღი გაიწეროს.
+default_availability_date = datetime.now()
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Properties"
-
-    # Fields below define the property settings
-
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
-    postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Date Availability')
-    expected_price = fields.Float(string='Expected Price', required=True)
-    selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
-    living_area = fields.Integer(string='Living Area')
-    facades = fields.Integer(string='Facades')
-    garage = fields.Boolean(string='Garage')
-    garden = fields.Boolean(string='Garden')
-    garden_area = fields.Integer(string='Garden Area')
-    garden_orientation = fields.Selection(
+    # ქვემოთ მოცემული Field-ებით ხდება უძრავი ქონების პარამეტრების გაწერა.
+    active = fields.Boolean(string='აქტიური', default=True)
+    state = fields.Selection(
+        string='განცხადების სტატუსი',
         selection=[
-            ('north', 'North'),
-            ('south', 'South'),
-            ('east', 'East'),
-            ('west', 'West')
+            ('ახალი','ახალი'),
+            ('გაყიდული','გაყიდული'),
+            ('გაუქმებული','გაუქმებული')
         ],
-        string='Garden Orientation'
+        required=True,
+        copy=False,
+        default='ახალი'
     )
+    name = fields.Char(string='სახელი', required=True)
+    description = fields.Text(string='აღწერა', required=True)
+    postcode = fields.Char(string='საფოსტო ინდექსი')
+    date_availability = fields.Date(string='განცხადების დამატების თარიღი', default=default_availability_date, copy=False)
+    selling_price = fields.Float(string='ფასი $', copy=False)
+    bedrooms = fields.Integer(string='საძინებლები', default='2')
+    living_area = fields.Integer(string='საცხოვრებელი არეალი მ²')
+    facades = fields.Integer(string='აივანი')
+    garage = fields.Boolean(string='გარაჟი')
+    garden = fields.Boolean(string='ეზო')
+    garden_area = fields.Integer(string='ეზოს არეალი მ²')
